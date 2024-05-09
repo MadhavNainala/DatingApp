ae52506d5788c5a5b5daefdcc10f49d23e472917
MadhavNainala <nainala.p@northeastern.edu>
2024-05-09 00:49:13 -0400

    UsersController

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (id, UserName)
+VALUES (1, "Bob");
+
+INSERT INTO Users (id, UserName)
+VALUES (2, "Jane");
+
+INSERT INTO Users (id, UserName)
+VALUES (3, "Rachel");
--- API/Program.cs
+++ API/Program.cs
@@ -7,17 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultString"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }) ;
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
