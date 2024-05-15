60108c5e877835949c2c966b966bef76cd4dba00
MadhavNainala <nainala.p@northeastern.edu>
2024-05-14 20:03:16 -0400

    Register/Home components

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,16 +11,27 @@ import { Component } from '@angular/core';
 
 export class NavComponent {
 
-  model : any = {}
+  model : any = {};
 
-  constructor(){
-    
+  constructor(public accountService: AccountService){
+
   }
   ngOnInit() : void{
+   
   } 
 
+  
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next : response => {
+              console.log(response);      
+      },
+      error : error => console.log(error) 
+    })
+  }
+
+  logout(){
+    this.accountService.logout();
   }
 
 }
